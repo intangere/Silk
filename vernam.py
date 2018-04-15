c5d9e3cc254ea93fe1333960c535e0b157b623c9
Ircd <ircd@plox.plox>
2018-04-15 10:25:02 +0200

    Cleaning up

--- vernam.py
+++ vernam.py
@@ -1,6 +1,6 @@
 def genVernamCipher( text, key ):
-    """ Returns the Vernam Cypher for given string and key """
-    answer = "" # the Cypher text
+    """ Returns the Vernam Cipher for given string and key """
+    answer = "" # the Cipher text
     p = 0 # pointer for the key
     for char in text:
         answer += chr(ord(char) ^ ord(key[p]))
